a9ab1768c28c32d99730cd01edc0eb802d8f54df
Adedokun Adeshina <adeshina93@gmail.com>
2024-09-15 01:13:34 -0700

    initial thing

---
+++ js/js_63hhph92hLMQLTCc9fBWnd9VpWrulSrdPGVSKG8-iWg.js
@@ -0,0 +1,2 @@
+/* @license GPL-2.0-or-later https://www.drupal.org/licensing/faq */
+(function(Drupal){Drupal.AjaxCommands.prototype.gtagEvent=function(ajax,response){gtag('event',response.event_name,response.data);};})(Drupal);;
